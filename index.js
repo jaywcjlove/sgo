fa08880dae0000854fc71507cd054c5a2a2512c1
jaywcjlove <398188662@qq.com>
2015-10-14 01:27:21 +0800

    检测port是否存在

--- index.js
+++ index.js
@@ -2,51 +2,105 @@ var http = require("http"),
     url = require("url"),
     path = require("path"),
     fs = require("fs"),
+    net = require("net"),
     color = require('colorful'),
-    port = 1987;
+    __port = 1987,
+    server;
 
 module.exports = server;
 
-function server(_port){
+connListener = function(request, response) {
 
-    port = _port || port;
+    var uri = url.parse(request.url).pathname, 
+        filename = path.join(process.cwd(), uri);
 
-    http.createServer(function(request, response) {
+    // url 解码
+    filename = decodeURIComponent(filename);
 
-        var uri = url.parse(request.url).pathname, 
-            filename = path.join(process.cwd(), uri);
+    console.log(color.green(" INFO "),filename);
 
-        // url 解码
-        filename = decodeURIComponent(filename);
+    fs.exists(filename, function(exists) {
+        if(!exists) {
+            response.writeHead(404, {"Content-Type": "text/plain"});
+            response.write("404 Not Found\n");
+            response.end();
+            return;
+        }
 
-        fs.exists(filename, function(exists) {
-            if(!exists) {
-                response.writeHead(404, {"Content-Type": "text/plain"});
-                response.write("404 Not Found\n");
+        if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+        fs.readFile(filename, "binary", function(err, file) {
+            if(err) {        
+                response.writeHead(500, {"Content-Type": "text/plain"});
+                response.write(err + "\n");
                 response.end();
                 return;
             }
 
-            if (fs.statSync(filename).isDirectory()) filename += '/index.html';
-
-            fs.readFile(filename, "binary", function(err, file) {
-                if(err) {        
-                    response.writeHead(500, {"Content-Type": "text/plain"});
-                    response.write(err + "\n");
-                    response.end();
-                    return;
-                }
-
-                response.writeHead(200);
-                response.write(file, "binary");
-                response.end();
-            });
-
+            response.writeHead(200);
+            response.write(file, "binary");
+            response.end();
         });
 
-    }).listen(parseInt(port, 10));
+    });
 
-    console.log("\n  Static file server running at" + color.green("\n\n=> http://localhost:" + port) + '\n');
-    // console.log("Server listening on: http://localhost:%s", PORT);
 }
 
+// 检测port是否存在
+function probe(port, callback) {
+
+    var server = net.createServer().listen(port)
+
+    var calledOnce = false
+
+    var timeoutRef = setTimeout(function () {
+        calledOnce = true
+        callback(false,port)
+    }, 2000)
+
+    timeoutRef.unref()
+
+    var connected = false
+
+    server.on('listening', function() {
+        clearTimeout(timeoutRef)
+
+        if (server)
+            server.close()
+
+        if (!calledOnce) {
+            calledOnce = true
+            callback(true,port)
+        }
+    })
+
+    server.on('error', function(err) {
+        clearTimeout(timeoutRef)
+
+        var result = true
+        if (err.code === 'EADDRINUSE')
+            result = false
+
+        if (!calledOnce) {
+            calledOnce = true
+            callback(result,port)
+        }
+    })
+}
+
+function server(_port){
+
+    var pt = _port || __port;
+
+    probe(pt,function(bl,_pt){
+        if(bl === true){
+            // ssr(_pt)
+            server = http.createServer(connListener);
+            server = server.listen(parseInt(_pt, 10));
+            console.log("\n  Static file server running at" + color.green("\n\n=> http://localhost:" + _pt ) + '\n');
+        }else{
+            server(_pt+1)
+        }
+    })
+
+}
