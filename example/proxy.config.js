ef7a86180dad20f72a13ccfdeea7958bc907980d
jaywcjlove <398188662@qq.com>
2016-06-18 02:37:50 +0800

    本地数据 mock例子添加

---
+++ example/proxy.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // Mock 数据返回
+  'GET /users': [{name:'kenny wang'}, {name:'JSLite doc'}],
+  'GET /users/1': {name:'卧槽'},
+  'POST /users':{name:'卧槽121221'},
+  'POST /users/2':"22323sd",
+};
