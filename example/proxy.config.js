86a1073ca281b2cb2fbfb81f2b040e65ad857842
jaywcjlove <398188662@qq.com>
2016-06-18 13:02:46 +0800

    update example, post parameter judgment.

--- example/proxy.config.js
+++ example/proxy.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  // Mock 数据返回
-  'GET /users': [{name:'kenny wang'}, {name:'JSLite doc'}],
-  'GET /users/1': {name:'卧槽'},
-  'POST /users':{name:'卧槽121221'},
-  'POST /users/2':"22323sd",
+    // Mock 数据返回
+    'GET /users': [{name:'kenny wang'}, {name:'JSLite doc'}],
+    'GET /users/1': {name:'卧槽'},
+    'POST /users':function(data,url){
+        console.log("data:::",data,data.a,url);
+        return {name:'卧槽121221'}
+    },
+    'POST /users/2':"22323sd",
 };
