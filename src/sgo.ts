88acbfb55fa6846c0dcf7c424f504bfb95c73804
jaywcjlove <398188662@qq.com>
2021-01-02 04:40:49 +0800

    fix: Fix argvs errors.

--- src/sgo.ts
+++ src/sgo.ts
@@ -40,11 +40,12 @@ import server from './server';
     console.log('  Copyright 2020');
     return;
   }
-
-  if (argvs.reload === undefined) argvs.reload = argvs.r = true;
-  if (argvs.browser === undefined) argvs.browser = argvs.b = true;
+  if (argvs.reload === undefined || argvs.r) argvs.reload = argvs.r = true;
+  if (argvs.browser === undefined || argvs.b) argvs.browser = argvs.b = true;
   if (!argvs.port) argvs.port = argvs.p = Number(process.env.PORT) || 1987;
+  if (argvs.p) argvs.port = argvs.p;
   if (!argvs.dir) argvs.dir = argvs.d = '';
+  if (argvs.d) argvs.dir = argvs.d;
   if (!argvs.fallback) argvs.fallback = '';
   if (!argvs['reload-port'])argvs['reload-port'] = 19872;
   server(argvs);
